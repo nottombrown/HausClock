c9333a89030795d37b6ccf883306da0f115bde76
Tom Brown <tom@joingrouper.com>
2014-11-12 18:29:46 -0800

    Paused view has a transparent background

---
+++ HausClock/PausedView.swift
@@ -0,0 +1,20 @@
+//
+//  PausedView.swift
+//  HausClock
+//
+//  Created by Tom Brown on 10/21/14.
+//  Copyright (c) 2014 not. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class PausedView: UIView {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.opaque = false
+        self.backgroundColor = UIKit.UIColor(white: 1.0, alpha: 0.0)
+    }
+}
